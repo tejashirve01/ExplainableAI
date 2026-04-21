8533035f08fcf97c041ef934233d0e1cf91b2789
frontends <tejashirve11@gmail.com>
2026-04-22 02:39:58 +0530

    embedder change

--- src/embedder.py
+++ src/embedder.py
@@ -1,18 +1,13 @@
 from sentence_transformers import SentenceTransformer
-
+import os
 
 class Embedder:
-
     def __init__(self):
-
         print("Loading embedding model...")
-
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
-
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        model_path = os.path.join(base_dir, "models", "all-MiniLM-L6-v2")
+        self.model = SentenceTransformer(model_path)
         print("Model loaded!")
 
     def embed(self, texts):
-
-        embeddings = self.model.encode(texts)
-
-        return embeddings
+        return self.model.encode(texts)
